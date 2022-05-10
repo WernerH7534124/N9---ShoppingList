03b41129c880900b202fa8c5fab154cafe217492
Werner Harak <wharak@tktk.ee>
2022-05-10 20:02:18 +0300

    Lisame N9 tööd

---
+++ List/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           {
+                string fileLocation = @"C:\Users\werne\source\samples\Aprill";
+                string fileName = @"\\Nädal20.txt";
+
+                int i = 0; 
+
+                string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+                List<string> poenimekiri = arrayFromFile.ToList<string>();
+
+                bool loopActive = true;
+
+                while (loopActive)
+                {
+                    Console.WriteLine("Kas lisame midagi nimekirja? Y/N:");
+                    char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                    if (userInput == 'y')
+                    {
+                        Console.WriteLine("Mida siis poest vaja? Pane kirja:");
+                        string ese = Console.ReadLine();
+                        poenimekiri.Add(ese);
+                    }
+                    else
+                    {
+                        loopActive = false;
+                        Console.WriteLine("Ja nüüd - Poodi!");
+                    }
+
+                    Console.Clear();
+
+                    Console.WriteLine($"Sinu poe nimekiri:");
+                    Console.WriteLine($"  ");
+
+                    foreach (string wish in poenimekiri)
+                    {
+                        Console.WriteLine($"{wish}");
+                    }
+                    Console.WriteLine($"  ");
+
+                    File.WriteAllLines($"{fileLocation}{fileName}", poenimekiri);
+                }
+    }
+
+    }
+
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Teeme uued poe nimekirjad?");
+            Console.WriteLine("  ");
+            string rootDirectory = @"C:\Users\werne\source\samples";
+            Console.WriteLine(" Sisesta kausta nimi:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Sisesta nimekirja nimetus ja lõppu .txt :");
+            string fileName = Console.ReadLine();
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist at {rootDirectory}");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+
+
+
+        }
+    }
+}
